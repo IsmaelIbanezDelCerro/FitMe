b920074b1e99cd15a48852532865ca44e5b8ee60
IsmaelIbanezDelCerro <ismatron2005@gmail.com>
2026-05-12 16:18:11 +0200

    Eliminar valoración de los gimnasios

--- app/src/main/java/com/example/fitme/place/PlaceResponse.kt
+++ app/src/main/java/com/example/fitme/place/PlaceResponse.kt
@@ -7,13 +7,11 @@ data class PlaceResponse(
     @SerializedName("name")    val name: String,
     @SerializedName("lat")     val lat: Double,
     @SerializedName("lng")     val lng: Double,
-    @SerializedName("address") val address: String,
-    @SerializedName("rating")  val rating: Double
+    @SerializedName("address") val address: String
 ) {
     fun toPlace() = Place(
         name = name,
         latLng = LatLng(lat, lng),
-        address = address,
-        rating = rating.toFloat()
+        address = address
     )
 }
